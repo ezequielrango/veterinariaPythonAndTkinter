f83cf8de8da313b4307c79fb94974ba2ae0550db
Nicolas <nicolasgalvez1985@gmail.com>
2023-09-27 16:33:28 -0300

    Se corrigen posiciones de botones

--- principal.py
+++ principal.py
@@ -12,56 +12,76 @@ def abrir_ventana_clientes():
         text="Volver", 
         command=ventana_secundaria.destroy
         )
-    boton_cerrar.place(x=75, y=75)
+    boton_cerrar.place(x=10, y=10)
     # Crear la ventana principal.
 
 def abrir_ventana_productos():
-        # Crear una ventana secundaria.
+        # Crear una ventana
     ventana_secundaria = tk.Toplevel()
     ventana_secundaria.title("Ventana productos")
     ventana_secundaria.config(width=300, height=200)
-        # Crear un botón dentro de la ventana secundaria
+        # Crear un botón dentro de la ventana
         # para cerrar la misma.
     boton_cerrar = ttk.Button(
         ventana_secundaria,
         text="Volver", 
         command=ventana_secundaria.destroy
         )
-    boton_cerrar.place(x=75, y=75)
+    boton_cerrar.place(x=10, y=10)
 
 def abrir_ventana_turnos():
-        # Crear una ventana secundaria.
+        # Crear una ventana.
     ventana_secundaria = tk.Toplevel()
     ventana_secundaria.title("Ventana Turnos")
     ventana_secundaria.config(width=300, height=200)
-        # Crear un botón dentro de la ventana secundaria
+        # Crear un botón dentro de la ventana
         # para cerrar la misma.
     boton_cerrar = ttk.Button(
         ventana_secundaria,
         text="Volver", 
         command=ventana_secundaria.destroy
         )
+    
+        # Boton para añadir turnos
     boton_anadir = ttk.Button(
         ventana_secundaria,
         text="Añadir", 
-        
+        command=abrir_ventana_anadir
         )
 
-    boton_cerrar.place(x=75, y=75)
-    boton_anadir.place(x=150, y=75)
-    # Crear la ventana principal.
+    boton_cerrar.place(x=10, y=10)
+    boton_anadir.place(x=220, y=170)
 
+def abrir_ventana_anadir():
+        # Crear una ventana
+    ventana_secundaria = tk.Toplevel()
+    ventana_secundaria.title("Ventana añadir")
+    ventana_secundaria.config(width=300, height=200)
+        # Crear un botón dentro de la ventana
+        # para cerrar la misma.
+    boton_cerrar = ttk.Button(
+        ventana_secundaria,
+        text="Volver", 
+        command=ventana_secundaria.destroy
+        )
+    boton_cerrar.place(x=10, y=10)
+
+
+    # Crear la ventana principal.
 ventana_principal = tk.Tk()
 ventana_principal.config(width=900, height=500)
 ventana_principal.title("Pets Python Code")
+
 # Crear un botón dentro de la ventana principal
 # que al ejecutarse invoca a la función
-# abrir_ventana_secundaria().
+# abrir_ventana_clientes.
 boton_abrirClientes = ttk.Button(
     ventana_principal,
     text="Clientes",
     command=abrir_ventana_clientes
 )
+
+# abrir_ventana_productos.
 
 boton_abrirProductos = ttk.Button(
     ventana_principal,
@@ -69,11 +89,14 @@ boton_abrirProductos = ttk.Button(
     command=abrir_ventana_productos
 )
 
+# abrir_ventana_turnos.
+
 boton_turnos = ttk.Button(
     ventana_principal,
     text="Turnos",
-    command=abrir_ventana_productos
+    command=abrir_ventana_turnos
 )
+
 
 boton_abrirClientes.place(x=300, y=100)
 boton_abrirProductos.place(x=500, y=100)
